67a0eaff7da1de461279acc3f83b22648f4bce5e
sava <savamedvedev@gmail.com>
2022-08-22 21:31:44 +0800

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,8 @@
+import User from "../models/User";
+import CryptoJS from "crypto-js";
+import jwt from "jsonwebtoken";
+
+export class AuthController {
+  async register() {}
+  async login() {}
+}
---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import chalk from "chalk";
+//////////////////////////////////////////////////////////////////
+
+// const userRoute = require("./routes/user");
+// const authRoute = require("./routes/auth");
+// const productRoute = require("./routes/product");
+// const cartRoute = require("./routes/cart");
+// const orderRoute = require("./routes/order");
+// const stripeRoute = require("./routes/stripe");
+//////////////////////////////////////////////////////////////////
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+const start = async () => {
+  try {
+    await mongoose
+      .connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((res) => console.log(chalk.bgBlue("DB Connect")))
+      .catch((err) => console.log(chalk.bgRed(err)));
+
+    app.listen(process.env.PORT, (err) => {
+      if (err) {
+        return console.log(chalk.bgRed(err));
+      }
+      console.log(chalk.bgBlue(`Starting Server on port ${process.env.PORT}`));
+    });
+  } catch (error) {
+    console.log(chalk.bgRed(error));
+  }
+};
+start();
