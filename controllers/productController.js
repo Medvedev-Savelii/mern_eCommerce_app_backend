5f35924daa2e77335f9a635a17a24997154ea1e5
sava <savamedvedev@gmail.com>
2022-08-30 23:24:43 +0800

    five-commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -26,7 +26,7 @@ export const update = async (req, res) => {
   }
 };
 
-export const deleted = async (req, res) => async (req, res) => {
+export const deleted = async (req, res) => {
   try {
     await Product.findByIdAndDelete(req.params.id);
     res.status(200).json("Product has been deleted...");
